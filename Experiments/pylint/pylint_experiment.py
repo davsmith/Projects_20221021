9357266e75f7bf94df7c93718e595f3c5f4ff8d1
Dave Smith <davsmith@users.noreply.github.com>
2021-06-07 18:13:05 -0700

    Added some variable values

--- Experiments/pylint/pylint_experiment.py
+++ Experiments/pylint/pylint_experiment.py
@@ -3,7 +3,7 @@ x2 = 2
 y = 15
 a = 10
 i = 7
-i2 = 7
+i2 = 7  # pylint: disable=invalid-name
 iz = 25
 z1 = 21
 z12 = 15
@@ -11,4 +11,7 @@ z111 = 111
 z = 'abc'
 zi = 123
 j = 456
+r = 2
+r1 = 4
+r2 = 4
 # print(x)

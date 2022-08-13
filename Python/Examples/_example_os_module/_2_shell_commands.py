b0607605f764c1836e62fe683e6b59c790ec090f
Dave Smith <noreply@davsmith.github.com>
2022-08-13 14:18:31 -0700

    Add example for os.popen and capturing output

--- Python/Examples/_example_os_module/_2_shell_commands.py
+++ Python/Examples/_example_os_module/_2_shell_commands.py
@@ -4,9 +4,18 @@
     Dave Smith
     August 12, 2022
 
+    Reference:
+    https://tinyurl.com/3jww9rry
+
+
 '''
 import os
  
+
+ #
+ # Use os.system to run commands where output is ignored
+ #
+
 # Print out the python version
 command = "python --version" #command to be executed
 
@@ -14,8 +23,12 @@ command = "python --version" #command to be executed
 res = os.system(command)
 print("Returned Value: ", res)
 
-# Create a set of files
-for i in range(1,11):
-    command = f"echo This is file {i} > file{i}.txt"
-    res = os.system(command)
-    print("Returned Value: ", res)
+#
+# Use os.popen to capture the output
+#
+myCmd = os.popen('python --version').read()
+print(myCmd)
+
+myCmd = os.popen('git status').read()
+results = myCmd.split('\n')
+print(results)

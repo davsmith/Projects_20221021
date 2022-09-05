681aaf392af865f5cadb5893ced14b1587d39b5e
Dave Smith <davsmith@users.noreply.github.com>
2022-09-05 10:01:06 -0700

    Update examples per second article

--- Python/Examples/_example_subprocess_module/shell_commands.py
+++ Python/Examples/_example_subprocess_module/shell_commands.py
@@ -2,6 +2,24 @@
 
 import subprocess
 
+# #
+# # Run shell commands using check_output()
+# #
+# # cmd = ['dir', '/s', 's*']
+# cmd = 'pingxxx -c google.com'
+# result = subprocess.check_output(cmd, text=True, shell=True)
+# print(result)
+
+#
+# Run shell commands using run()
+#
+result = subprocess.run(['ping', 'google.com'], capture_output=True)
+print(f"STDOUT:\n{result.stdout}\n\n")
+print(f"STDERR:\n{result.stderr}\n\n")
+print(f"Exit code: {result.returncode}")
+print(f"Args: {result.args}")
+
+
 
 # #
 # # Example 1
@@ -21,31 +39,31 @@ import subprocess
 # # Print the content of sp variable
 # print(sp)
 
-#
-# Example 2
-#
-# 
+# #
+# # Example 2
+# #
+# # 
 
-# Define command as string and then split() into list format
-cmd = 'ping google.com'
+# # Define command as string and then split() into list format
+# cmd = 'ping google.com'
 
-# Use shell to execute the command, store the stdout and stderr in sp variable
-sp = subprocess.Popen(cmd,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True)
+# # Use shell to execute the command, store the stdout and stderr in sp variable
+# sp = subprocess.Popen(cmd,
+#         shell=True,
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#         universal_newlines=True)
 
-# Store the return code in rc variable
-rc = sp.wait()
+# # Store the return code in rc variable
+# rc = sp.wait()
 
-# Separate the output and error by communicating with sp variable.
-# This is similar to Tuple where we store two values to two different variables
-out,err = sp.communicate()
+# # Separate the output and error by communicating with sp variable.
+# # This is similar to Tuple where we store two values to two different variables
+# out,err = sp.communicate()
 
-print('Return Code:',rc,'\n')
-print('output is: \n', out)
-print('error is: \n', err)
+# print('Return Code:',rc,'\n')
+# print('output is: \n', out)
+# print('error is: \n', err)
 
 # #
 # # Example 3

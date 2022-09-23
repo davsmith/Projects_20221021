a7134d17b6a778a79d39d7c437cc4987467f25a8
Dave Smith <noreply@davsmith.github.com>
2022-09-22 22:26:48 -0700

    Add repos to build_example_repos

--- Python/repo_builder/build_example_repos.py
+++ Python/repo_builder/build_example_repos.py
@@ -4,9 +4,15 @@ import example_conflicts
 import example_complicated
 import example_fast_forward
 import example_simple
+import example_three_branch
+import example_multiple_branches
+import example_split_branch
 
 example_empty_repo.build('1_empty_repo')
 example_conflicts.build('2_conflicts')
 example_complicated.build('3_complicated')
 example_fast_forward.build('4_fast_forward')
 example_simple.build('5_simple')
+example_three_branch.build('6_three_branch')
+example_multiple_branches.build('7_multi_branch')
+example_split_branch.build('8_split')

cc52b54313bba35b77240ba9cc96e6437c53e23e
Dave Smith <davsmith@users.noreply.github.com>
2022-09-25 07:11:57 -0700

    Changed scratch branch

--- Python/repo_builder/example_scratch.py
+++ Python/repo_builder/example_scratch.py
@@ -4,17 +4,16 @@ from repo_builder import Repo
 
 def build(repo_name, parent_folder=None):
     ''' Build an experimental test repo '''
+    # Based on the three_branch example repo
+
     if parent_folder is None:
         parent_folder = 'c:/temp'
 
     # Create a new repo with multiple branches, no conflicts
     repo = Repo(repo_name, parent_folder)
-    repo.create_repo(first_branch='main', num_commits=2)
-    repo.create_branch(branch='feature1', from_branch='main', num_commits=0)
-    repo.create_branch(branch='feature2', from_branch='main', num_commits=0)
-    repo.add_commits(num_commits=3, branch='feature1')
-    repo.add_commits(num_commits=3, branch='feature2')
-    repo.add_commits(num_commits=2, branch='main')
+    repo.create_repo(initial_branch='main', num_commits=2)
+    repo.file_builder.touch_files(count=1)
+    repo.commit_files(comment='M')
 
 if __name__ == '__main__':
-    build('three_branch')
+    build('scratch')

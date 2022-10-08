969910895c6e4201e09f31de56032dbb463d5c91
Dave Smith <noreply@davsmith.github.com>
2022-10-07 17:42:58 -0700

    Add build method to multiple_merge.py

--- Python/repo_builder/build_example_repos.py
+++ Python/repo_builder/build_example_repos.py
@@ -6,13 +6,15 @@ import example_fast_forward
 import example_simple
 import example_three_branch
 import example_multiple_branches
+import example_multiple_merge
 import example_split_branch
 
 example_empty_repo.build('1_empty_repo')
-example_conflicts.build('2_conflicts')
-example_complicated.build('3_complicated')
-example_fast_forward.build('4_fast_forward')
-example_simple.build('5_simple')
-example_three_branch.build('6_three_branch')
-example_multiple_branches.build('7_multi_branch')
-example_split_branch.build('8_split')
+example_fast_forward.build('2_fast_forward')
+example_simple.build('3_simple')
+example_split_branch.build('4_split')
+example_three_branch.build('5_three_branch')
+example_multiple_branches.build('6_multi_branch')
+example_multiple_merge.build('7_multi_merge')
+example_conflicts.build('8_conflicts')
+example_complicated.build('9_complicated')
